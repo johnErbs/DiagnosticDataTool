0be4a3d4de99fea21b26682f820f3b879f867890
johnnyschmidterbs <47756660+johnnyschmidterbs@users.noreply.github.com>
2021-02-16 13:54:02 +0100

    monitor (TDD)

---
+++ MediaWareMonitor/MediaWareMonitor/LoggMonitor.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaWareMonitor
+{
+    class LoggMonitor : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private static DateTime dt = new DateTime();
+        public static DateTime DT
+        {
+            get { return dt; }
+            set { dt = value; }
+        }
+
+        private string time;
+        public string Time
+        {
+            get { return time; }
+            set
+            {
+                time = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string date;
+        public string Date
+        {
+            get { return date; }
+            set
+            {
+                date = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private string hardwareInfo;
+        public string HardwareInfo
+        {
+            get { return hardwareInfo; }
+            set
+            {
+                hardwareInfo = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        // This method is called by the Set accessor of each property.
+        // The CallerMemberName attribute that is applied to the optional propertyName
+        // parameter causes the property name of the caller to be substituted as an argument.
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ MediaWareMonitor/MediaWareMonitor/Pages/LeftSidePanel.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MediaWareMonitor.Pages
+{
+    /// <summary>
+    /// Interaction logic for LeftSidePanel.xaml
+    /// </summary>
+    public partial class LeftSidePanel : Page
+    {
+        
+        public LeftSidePanel()
+        {
+            InitializeComponent();
+            Hardware.Cpu = false;
+            Hardware.Memory = false;
+            Hardware.Tempprature = false;
+            Chart.EnableLineChart = true;
+            
+        }
+
+        private void btn_Click_Ram(object sender, RoutedEventArgs e)
+        {
+            Chart.ClearChart = true;
+            Chart.EnableLineChart = true;
+            Hardware.Cpu = false;
+            Hardware.Tempprature = false;
+            Hardware.Memory = true;
+        }
+
+        private void btn_Click_Cpu(object sender, RoutedEventArgs e)
+        {
+            Chart.ClearChart = true;
+            Chart.EnableLineChart = true;
+            Hardware.Memory = false;
+            Hardware.Tempprature = false;
+            Hardware.Cpu = true;
+        }
+        private void btn_Click_CpuTemprature(object sender, RoutedEventArgs e)
+        {
+            Chart.ClearChart = true;
+            Chart.EnableLineChart = true;
+            Hardware.Memory = false;
+            Hardware.Cpu = false;
+            Hardware.Tempprature = true;
+        }
+    }
+}
